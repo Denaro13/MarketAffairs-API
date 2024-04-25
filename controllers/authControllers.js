c70d916342852948c5edad065235919a7f0e0864
Usman Nurudeen <usmannurudeen13@gmail.com>
2024-04-25 02:30:48 +0100

    Authentication complete

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -20,10 +20,30 @@ const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+  const validPassword = await user.comparePassword(password);
+  if (!validPassword) {
+    throw new CustomError.NotFoundError("Invalid Password");
+  }
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 const logout = async (req, res) => {
-  res.send("logout user");
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: "User logged Out!" });
 };
 
 module.exports = { register, login, logout };
