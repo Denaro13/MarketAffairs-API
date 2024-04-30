f95a7cc211aff0a66383f2c287e783bd269ba9d8
Usman Nurudeen <usmannurudeen13@gmail.com>
2024-04-30 11:31:18 +0100

    Product Routes Completed

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 //rest of packages
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 //ConnectDB
 const connectDB = require("./db/connect");
@@ -14,6 +15,7 @@ const connectDB = require("./db/connect");
 //routers
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
 
 //Middleware
 const notFoundMiddleWare = require("./middleware/not-found");
@@ -22,6 +24,8 @@ const errorHandlerMiddleWare = require("./middleware/error-handler");
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+app.use(express.static("./public"));
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
   res.send("e-commerce api");
@@ -32,6 +36,7 @@ app.get("/api/v1", (req, res) => {
 });
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
 
 app.use(notFoundMiddleWare);
 app.use(errorHandlerMiddleWare);
---
+++ models/Product.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+const { trim } = require("validator");
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide Product name"],
+      maxLength: [60, "Product name cannot be more than 60 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please provide Product price"],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide Product description"],
+      maxLength: [
+        1000,
+        "Product description cannot be more than 1000 characters",
+      ],
+    },
+    image: {
+      type: String,
+      default: "/uploads/example.jpeg",
+    },
+    category: {
+      type: String,
+      required: [true, "Please provide product category"],
+      enum: ["office", "kitchen", "bedroom"],
+    },
+    company: {
+      type: String,
+      required: [true, "Please provide company"],
+      enum: {
+        values: ["ikea", "liddy", "marcos"],
+        msg: "{VALUE} is not supported",
+      },
+    },
+    colors: {
+      type: [String],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
