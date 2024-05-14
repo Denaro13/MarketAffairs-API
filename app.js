68ee926b653c31696eff1e96a4e2434a5902e8a7
Usman Nurudeen <usmannurudeen13@gmail.com>
2024-05-14 07:02:54 +0100

    Review Controller

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const connectDB = require("./db/connect");
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const productRouter = require("./routes/productRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
 
 //Middleware
 const notFoundMiddleWare = require("./middleware/not-found");
@@ -37,6 +38,7 @@ app.get("/api/v1", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 app.use(notFoundMiddleWare);
 app.use(errorHandlerMiddleWare);
---
+++ models/Review.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      require: [true, "Please provide rating"],
+    },
+    title: {
+      type: String,
+      trim: true,
+      require: [true, "Please provide review title"],
+      maxLength: 100,
+    },
+    comment: {
+      type: String,
+      require: [true, "Please provide review text"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      require: true,
+    },
+  },
+  { timeStamps: true }
+);
+
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+module.exports = mongoose.model("Review", ReviewSchema);
