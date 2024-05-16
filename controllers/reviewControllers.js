e17fb0836ea5b7cf8145a7715ca95d840c6a4e91
Usman Nurudeen <usmannurudeen13@gmail.com>
2024-05-16 23:42:01 +0100

    Create Review

--- controllers/reviewControllers.js
+++ controllers/reviewControllers.js
@@ -1,9 +1,27 @@
 const Review = require("../models/Review");
+const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 
 const createReview = async (req, res) => {
-  res.send("create review");
+  const { product: productId } = req.body;
+
+  const productExist = await Product.findOne({ _id: productId });
+  if (!productExist) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+
+  const userAlreadyReviewed = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+
+  if (userAlreadyReviewed) {
+    throw new CustomError.BadRequestError("User already reviewed");
+  }
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
+  res.status(StatusCodes.CREATED).json({ review });
 };
 const getAllReviews = async (req, res) => {
   res.send("get all reviews");
